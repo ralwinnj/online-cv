602f9d5f1047058536ba93cd7227b368773e31e0
Ralwinn Johnson <RalwinnJ@buffalocity.gov.za>
2019-07-22 13:46:01 +0200

    updated api.service.ts with new api end points

--- src/app/shared/api.service.ts
+++ src/app/shared/api.service.ts
@@ -1,5 +1,6 @@
-import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+import { Injectable } from '@angular/core';
 
 const headers = new HttpHeaders()
   .set("X-Requested-With", "XMLHttpRequest")
@@ -19,56 +20,232 @@ export class ApiService {
     this.API_URL = 'https://localhost:44351/api/';
   }
 
-  postApplicantDocument(data) {
-    return this.http.post(`${this.API_URL}applicantdocuments` ,data, reqOptions);
+
+  postApplicantDocument(id, data) {
+    return this.http.post(`${this.API_URL}applicantdocuments/${id}`, data, reqOptions);
   }
-  getApplicantDocumnent(id){
+  getApplicantDocumnent(id) {
     return this.http.get(`${this.API_URL}applicantdocuments/${id}`, reqOptions);
   }
-  putApplicantDocumnent(id, data){
+  putApplicantDocumnent(id, data) {
     return this.http.put(`${this.API_URL}applicantdocuments/${id}`, data, reqOptions);
   }
-  deleteApplicantDocumnent(id){
+  deleteApplicantDocumnent(id) {
     return this.http.delete(`${this.API_URL}applicantdocuments/${id}`, reqOptions);
   }
 
-  postApplicant(data) {
-    return this.http.post(`${this.API_URL}applicants` ,data, reqOptions);
+
+  postApplicant(id, data) {
+    return this.http.post(`${this.API_URL}applicants/${id}`, data, reqOptions);
   }
-  getApplicants(){
-    return this.http.get(`${this.API_URL}applicants`, reqOptions);
+  getApplicants(id?) {
+    if (id) {
+      return this.http.get(`${this.API_URL}applicants/${id}`, reqOptions);
+    } else {
+      return this.http.get(`${this.API_URL}applicants`, reqOptions);
+    }
   }
-  getApplicant(id){
-    return this.http.get(`${this.API_URL}applicants/${id}`, reqOptions);
-  }
-  putApplicant(id, data){
+  putApplicant(id, data) {
     return this.http.put(`${this.API_URL}applicants/${id}`, data, reqOptions);
   }
-  deleteApplicant(id){
+  deleteApplicant(id) {
     return this.http.delete(`${this.API_URL}applicants/${id}`, reqOptions);
   }
-  
 
-  postApplicantVacancy(data) {
+
+  postApplicantVacancy(id, data) {
     return this.http.post(`${this.API_URL}applicantvacancies`, data, reqOptions);
   }
-  getApplicantVacancies() {
-    return this.http.post(`${this.API_URL}applicantvacancies`, reqOptions);
+  getApplicantVacancies(id?) {
+    if (id) {
+      return this.http.get(`${this.API_URL}applicantvacancies/${id}`);
+    } else {
+      return this.http.post(`${this.API_URL}applicantvacancies`, reqOptions);
+    }
   }
-  getVacancies() {
-    return this.http.get(`${this.API_URL}applicantvacancies`, reqOptions);
+
+  postComputerLiteracy(id, data) {
+    return this.http.post(`${this.API_URL}computerliteracies/${id}`, data, reqOptions)
   }
-  getVacancy(id) {
-    return this.http.get(`${this.API_URL}applicantvacancies/${id}`, reqOptions);
+  getComputerLiteracies(id?) {
+    if (id) {
+      return this.http.get(`${this.API_URL}computerliteracies/${id}`, reqOptions);
+    } else {
+      return this.http.get(`${this.API_URL}computerliteracies`, reqOptions);
+    }
   }
-  
+  putComputerLiteracies(id, data) {
+    return this.http.put(`${this.API_URL}computerliteracies`, data,reqOptions );
+  }
+
+  /* Criminal Record API */
+  getCriminalRecords(id?) {
+    if (id) {
+      return this.http.get(`${this.API_URL}criminalrecords/${id}`, reqOptions);
+    } else {
+      return this.http.get(`${this.API_URL}criminalrecords`);
+    }
+  }
+  postCriminalRecords(id, data) {
+    return this.http.post(`${this.API_URL}/criminalrecords/${id}`, data, reqOptions);
+  }
+  putCriminalRecords(id, data){
+    return this.http.post(`${this.API_URL}/criminalrecords/${id}`, data, reqOptions);
+  }
+  deleteCriminalRecords(id){
+    return this.http.delete(`${this.API_URL}/criminalrecords/${id}`, reqOptions);
+  }
+
+  /* Disciplinary Record API */
+  getDisciplinaryRecords(id?) {
+    if (id) {
+      return this.http.get(`${this.API_URL}disciplinary/${id}`, reqOptions);
+    } else {
+      return this.http.get(`${this.API_URL}disciplinary`);
+    }
+  }
+  postDisciplinaryRecords(id, data) {
+    return this.http.post(`${this.API_URL}/disciplinary/${id}`, data, reqOptions);
+  }
+  putDisciplinaryRecords(id, data){
+    return this.http.post(`${this.API_URL}/disciplinary/${id}`, data, reqOptions);
+  }
+  deleteDisciplinaryRecords(id){
+    return this.http.delete(`${this.API_URL}/disciplinary/${id}`, reqOptions);
+  }
+
+  /* Experiences API */
+  getExperiences(id?) {
+    if (id) {
+      return this.http.get(`${this.API_URL}experiences/${id}`, reqOptions);
+    } else {
+      return this.http.get(`${this.API_URL}experiences`);
+    }
+  }
+  postExperiences(id, data) {
+    return this.http.post(`${this.API_URL}/experiences/${id}`, data, reqOptions);
+  }
+  putExperiences(id, data){
+    return this.http.post(`${this.API_URL}/experiences/${id}`, data, reqOptions);
+  }
+  deleteExperiences(id){
+    return this.http.delete(`${this.API_URL}/experiences/${id}`, reqOptions);
+  }
+
+  /* Generals API */
+  getGenerals(id?) {
+    if (id) {
+      return this.http.get(`${this.API_URL}generals/${id}`, reqOptions);
+    } else {
+      return this.http.get(`${this.API_URL}generals`);
+    }
+  }
+  postGenerals(id, data) {
+    return this.http.post(`${this.API_URL}/generals/${id}`, data, reqOptions);
+  }
+  putGenerals(id, data){
+    return this.http.post(`${this.API_URL}/generals/${id}`, data, reqOptions);
+  }
+  deleteGenerals(id){
+    return this.http.delete(`${this.API_URL}/generals/${id}`, reqOptions);
+  }
+
+
+
+  /* ProfessionalMemberships API */
+  getProfessionalMemberships(id?) {
+    if (id) {
+      return this.http.get(`${this.API_URL}professsionalmemberships/${id}`, reqOptions);
+    } else {
+      return this.http.get(`${this.API_URL}professsionalmemberships`);
+    }
+  }
+  postProfessionalMemberships(id, data) {
+    return this.http.post(`${this.API_URL}/professsionalmemberships/${id}`, data, reqOptions);
+  }
+  putProfessionalMemberships(id, data){
+    return this.http.post(`${this.API_URL}/professsionalmemberships/${id}`, data, reqOptions);
+  }
+  deleteProfessionalMemberships(id){
+    return this.http.delete(`${this.API_URL}/professsionalmemberships/${id}`, reqOptions);
+  }
+
+  /* Qualifications API */
+  getQualifications(id?) {
+    if (id) {
+      return this.http.get(`${this.API_URL}qualifications/${id}`, reqOptions);
+    } else {
+      return this.http.get(`${this.API_URL}qualifications`);
+    }
+  }
+  postQualifications(id, data) {
+    return this.http.post(`${this.API_URL}/qualifications/${id}`, data, reqOptions);
+  }
+  putQualifications(id, data){
+    return this.http.post(`${this.API_URL}/qualifications/${id}`, data, reqOptions);
+  }
+  deleteQualifications(id){
+    return this.http.delete(`${this.API_URL}/qualifications/${id}`, reqOptions);
+  }
+
+  /* Reference API */
+  getReference(id?) {
+    if (id) {
+      return this.http.get(`${this.API_URL}references/${id}`, reqOptions);
+    } else {
+      return this.http.get(`${this.API_URL}references`);
+    }
+  }
+  postReference(id, data) {
+    return this.http.post(`${this.API_URL}/references/${id}`, data, reqOptions);
+  }
+  putReference(id, data){
+    return this.http.post(`${this.API_URL}/references/${id}`, data, reqOptions);
+  }
+  deleteReference(id){
+    return this.http.delete(`${this.API_URL}/references/${id}`, reqOptions);
+  }
+
+  /* Vacancies API */
+  getVacancies(id?) {
+    if (id) {
+      return this.http.get(`${this.API_URL}vacancies/${id}`, reqOptions);
+    } else {
+      return this.http.get(`${this.API_URL}vacancies`);
+    }
+  }
+  postVacancies(data) {
+    return this.http.post(`${this.API_URL}/vacancies`, data, reqOptions);
+  }
+  deleteVacancies(id){
+    return this.http.delete(`${this.API_URL}/vacancies/${id}`, reqOptions);
+  }
+
+
+
+  /* Users */
+  createUser(data) {
+    return this.http.post(`${this.API_URL}users`, data, reqOptions);
+  }
+  deleteUser(id) {
+    return this.http.delete(`${this.API_URL}users`, reqOptions);
+  }
+
+  /* Login API */
+  ValidateLogin1(data) {
+    return this.http.post(`${this.API_URL}logins`, data, reqOptions);
+  }
+  updatePassword1(id, data){
+    console.log('TODO add this code soon!');
+  }
+
 
   createLogin(data?) {
     return this.http.post(`${this.API_URL}users`, data, reqOptions);
   }
 
   ValidateLogin(data) {
-    return this.http.post(`${this.API_URL}validatelogin`, data, reqOptions);
+    return this.http.post(`${this.API_URL}login`, data, reqOptions);
   }
 
   GetApplicant(body) {
