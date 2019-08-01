b38ded604953aa8cf2ef3e925dc1808df16c9cc9
Ralwinn Johnson <RalwinnJ@buffalocity.gov.za>
2019-08-01 11:05:07 +0200

    Commit

--- src/app/pipes/sort-by.pipe.ts
+++ src/app/pipes/sort-by.pipe.ts
@@ -7,13 +7,8 @@ import * as _ from 'lodash';
 export class SortByPipe implements PipeTransform {
 
   transform(value: any[], column: string, order = ''): any[] {
-    
-    console.log('Column is: ', column);
-    console.log('Value is: ', value);
-    console.log('order is: ', order)
     if (!value || !column || column === '' || order === '') { return value; } // no array
     if (value.length <= 1) { return value; } // array with only one item
     return _.orderBy(value, [column], [order]);
   }
-
 }
